0457b4f453e492fc7998fdb83321057b12ed23b8
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2025-05-17 18:24:51 +0300

    sharing media cleanup

--- features/media-view/src/main/java/com/furianrt/mediaview/internal/ui/composables/Toolbar.kt
+++ features/media-view/src/main/java/com/furianrt/mediaview/internal/ui/composables/Toolbar.kt
@@ -5,41 +5,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.systemGestureExclusion
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.LifecycleStartEffect
-import com.furianrt.mediaview.R
 import com.furianrt.uikit.components.ButtonBack
 import com.furianrt.uikit.components.ButtonMenu
-import com.furianrt.uikit.components.MenuItem
 import com.furianrt.uikit.constants.ToolbarConstants
 import com.furianrt.uikit.extensions.clickableNoRipple
-import com.furianrt.uikit.theme.Colors
 import com.furianrt.uikit.theme.SerenityTheme
-import com.furianrt.uikit.utils.LocalAuth
-import dev.chrisbanes.haze.HazeDefaults
 import dev.chrisbanes.haze.HazeState
-import dev.chrisbanes.haze.HazeTint
-import dev.chrisbanes.haze.hazeChild
-import kotlinx.coroutines.launch
 import com.furianrt.uikit.R as uiR
 
 @Composable
