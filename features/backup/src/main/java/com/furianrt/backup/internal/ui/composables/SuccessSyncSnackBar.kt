ae26d1c32e07f09307a960b780d9f95d92c49544
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2025-07-10 12:50:17 +0300

    added media sync success anim

--- features/backup/src/main/java/com/furianrt/backup/internal/ui/composables/SuccessSyncSnackBar.kt
+++ features/backup/src/main/java/com/furianrt/backup/internal/ui/composables/SuccessSyncSnackBar.kt
@@ -1,26 +1,31 @@
 package com.furianrt.backup.internal.ui.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.furianrt.backup.R
+import com.furianrt.uikit.extensions.clickableNoRipple
 import com.furianrt.uikit.theme.SerenityTheme
 import com.furianrt.uikit.utils.PreviewWithBackground
 import dev.chrisbanes.haze.HazeDefaults
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.HazeTint
 import dev.chrisbanes.haze.hazeEffect
-import com.furianrt.uikit.R as uiR
 
 @Composable
 internal fun SuccessSyncSnackBar(
@@ -28,6 +33,11 @@ internal fun SuccessSyncSnackBar(
     hazeState: HazeState,
     modifier: Modifier = Modifier,
 ) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.anim_sync_success),
+    )
+    val lottieState = animateLottieCompositionAsState(composition = composition)
+
     Column(
         modifier = modifier
             .widthIn(min = 200.dp)
@@ -40,18 +50,19 @@ internal fun SuccessSyncSnackBar(
                     blurRadius = 16.dp,
                 )
             )
+            .clickableNoRipple {}
             .padding(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        Icon(
+        LottieAnimation(
             modifier = Modifier.size(64.dp),
-            painter = painterResource(uiR.drawable.ic_action_done),
-            tint = MaterialTheme.colorScheme.primary,
-            contentDescription = null,
+            composition = composition,
+            progress = { lottieState.progress },
         )
         Text(
             text = title,
-            style = MaterialTheme.typography.titleLarge,
+            style = MaterialTheme.typography.titleMedium,
         )
     }
 }
