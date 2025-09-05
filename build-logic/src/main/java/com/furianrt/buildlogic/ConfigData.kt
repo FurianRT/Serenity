a62fe58bafdcc4add702d4f1c9028c36c86cf53d
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2025-09-05 13:45:36 +0500

    prepare release 1.3.3

--- build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
+++ build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
@@ -7,8 +7,8 @@ object ConfigData {
     const val COMPILE_SDK_VERSION = 36
     const val MIN_SDK_VERSION = 33
     const val TARGET_SDK_VERSION = 36
-    const val VERSION_CODE = 15
-    const val VERSION_NAME = "1.3.2"
+    const val VERSION_CODE = 16
+    const val VERSION_NAME = "1.3.3"
     val JVM_TARGET = JvmTarget.JVM_17
     val JAVA_VERSION = JavaVersion.VERSION_17
 }
