4265e73868902824c2da3a260c3c9e52d69ded0b
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-01-28 16:03:18 +0300

    prepare release 1.8.0

--- build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
+++ build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
@@ -1,16 +1,14 @@
 package com.furianrt.buildlogic
 
 import org.gradle.api.JavaVersion
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object ConfigData {
     const val COMPILE_SDK_VERSION = 36
     const val MIN_SDK_VERSION = 33
     const val TARGET_SDK_VERSION = 36
 
-    const val VERSION_CODE = 28
-    const val VERSION_NAME = "1.7.2"
+    const val VERSION_CODE = 29
+    const val VERSION_NAME = "1.8.0"
 
-    val JVM_TARGET = JvmTarget.JVM_17
     val JAVA_VERSION = JavaVersion.VERSION_17
 }
