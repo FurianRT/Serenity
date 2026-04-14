f5983469ba3a12cf90d4612c325da024fcf53f9d
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-04-14 14:58:40 +0300

    cleanup

--- features/backup/src/main/java/com/furianrt/backup/internal/domain/usecases/SignInUseCase.kt
+++ features/backup/src/main/java/com/furianrt/backup/internal/domain/usecases/SignInUseCase.kt
@@ -1,7 +1,6 @@
 package com.furianrt.backup.internal.domain.usecases
 
 import android.content.Intent
-import com.furianrt.backup.internal.domain.entities.AuthResult
 import com.furianrt.backup.internal.domain.exceptions.AuthException
 import com.furianrt.backup.internal.domain.repositories.BackupRepository
 import com.furianrt.domain.entities.BackupProfile
