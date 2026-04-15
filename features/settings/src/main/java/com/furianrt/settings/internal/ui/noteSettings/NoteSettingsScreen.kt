4ef05962b1f592a3218512f8c09b149c4cc29d28
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-04-15 21:39:52 +0300

    note settings scroll fix

--- features/settings/src/main/java/com/furianrt/settings/internal/ui/noteSettings/NoteSettingsScreen.kt
+++ features/settings/src/main/java/com/furianrt/settings/internal/ui/noteSettings/NoteSettingsScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -100,6 +102,7 @@ private fun SuccessContent(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(8.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
