f069939a66c32892c89e1ec0e22dcc287d6f9225
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-04-24 20:39:23 +0300

    api 37

--- build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
+++ build-logic/src/main/java/com/furianrt/buildlogic/ConfigData.kt
@@ -3,9 +3,9 @@ package com.furianrt.buildlogic
 import org.gradle.api.JavaVersion
 
 object ConfigData {
-    const val COMPILE_SDK_VERSION = 36
+    const val COMPILE_SDK_VERSION = 37
     const val MIN_SDK_VERSION = 33
-    const val TARGET_SDK_VERSION = 36
+    const val TARGET_SDK_VERSION = 37
 
     const val VERSION_CODE = 55
     const val VERSION_NAME = "2.6.1"
