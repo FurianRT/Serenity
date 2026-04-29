23776fa18785b80d29e575043918247ee296ccfa
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-04-29 18:06:59 +0300

    cleanup

--- features/backup/src/main/java/com/furianrt/backup/internal/ui/composables/QuestionsList.kt
+++ features/backup/src/main/java/com/furianrt/backup/internal/ui/composables/QuestionsList.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.furianrt.backup.R
