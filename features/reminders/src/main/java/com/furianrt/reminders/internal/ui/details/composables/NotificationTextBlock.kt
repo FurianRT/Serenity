b150de8149c12da88f62a4a073068cf2109cf836
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-05-24 20:24:14 +0300

    ui fixes

--- features/reminders/src/main/java/com/furianrt/reminders/internal/ui/details/composables/NotificationTextBlock.kt
+++ features/reminders/src/main/java/com/furianrt/reminders/internal/ui/details/composables/NotificationTextBlock.kt
@@ -3,15 +3,16 @@ package com.furianrt.reminders.internal.ui.details.composables
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -36,10 +37,9 @@ internal fun NotificationTextBlock(
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.background)
             .clickable(onClick = onClick)
-            .padding(start = 16.dp, end = 8.dp, top = 12.dp, bottom = 12.dp)
+            .padding(start = 16.dp, end = 8.dp, top = 14.dp, bottom = 14.dp)
             .animateContentSize(),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         Text(
             modifier = Modifier
@@ -48,19 +48,21 @@ internal fun NotificationTextBlock(
             text = text.ifEmpty { stringResource(R.string.reminders_notification_text_hint) },
             style = MaterialTheme.typography.bodyMedium,
         )
-        IconButton(
-            onClick = { if (text.isEmpty()) onClick() else onClearClick() },
-        ) {
-            Icon(
-                modifier = Modifier.alpha(0.5f),
-                painter = if (text.isEmpty()) {
-                    painterResource(uiR.drawable.ic_action_edit)
-                } else {
-                    painterResource(uiR.drawable.ic_exit)
-                },
-                tint = MaterialTheme.colorScheme.onSurface,
-                contentDescription = null,
-            )
+        CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 40.dp) {
+            IconButton(
+                onClick = { if (text.isEmpty()) onClick() else onClearClick() },
+            ) {
+                Icon(
+                    modifier = Modifier.alpha(0.5f),
+                    painter = if (text.isEmpty()) {
+                        painterResource(uiR.drawable.ic_action_edit)
+                    } else {
+                        painterResource(uiR.drawable.ic_exit)
+                    },
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    contentDescription = null,
+                )
+            }
         }
     }
 }
