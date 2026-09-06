3e687410edde3149cc7cf25e09a307287336c28e
Konstantin Cherkasov <konstantin.cherkasov@lamoda.ru>
2026-09-06 10:54:42 +0300

    prepare release 2.12.0

--- build-logic/src/main/java/com/furianrt/buildlogic/convention.android-app.gradle.kts
+++ build-logic/src/main/java/com/furianrt/buildlogic/convention.android-app.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.furianrt.serenity"
         minSdk = 33
         targetSdk = 37
-        versionCode = 78
-        versionName = "2.11.4"
+        versionCode = 79
+        versionName = "2.12.0"
     }
 
     buildFeatures {
